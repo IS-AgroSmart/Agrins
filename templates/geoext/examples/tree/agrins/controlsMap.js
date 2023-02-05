2e6b399d871027b6dd51c2b414a6a2977ebaf824
Udannyf <danny_u89@hotmail.com>
2023-02-05 05:12:45 -0500

    create_feature_json

--- templates/geoext/examples/tree/agrins/controlsMap.js
+++ templates/geoext/examples/tree/agrins/controlsMap.js
@@ -1,3 +1,37 @@
+let interactionLayer, interactionSource, popup, selectClick;
+var featureType = 'LineString';
+var helpTooltipElement, helpTooltip;
+var measureTooltipElement, measureTooltip;
+var numMeasurement = 0;
+var measureTooltips = {};
+var draw, listener, isDrawing = false;
+var delete_handler;
+var drawingFeature = null;
+var helpMsg;
+
+function addMeasureInteraction() {
+    interactionSource = new ol.source.Vector();
+    interactionLayer = new ol.layer.Vector({
+        source: interactionSource,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#ffcc33',
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 5,
+                fill: new ol.style.Fill({
+                    color: '#ffcc33'
+                }),
+            }),
+        }),
+    });
+    olMap.addLayer(interactionLayer);
+}
+
 function addControlsMap(){
     addMeasureInteraction();
     ctrlSwiper = new ol.control.Swipe({position: -0.5});
@@ -68,56 +102,11 @@ function addControlsMap(){
         }
     });
 
-    var searchBarlocal= document.getElementById('searchBar')
-
-
-    //let SaveMeasurementsControl = createSaveControl();
-    //olMap.addControl(new SaveMeasurementsControl());
-    //let ClearMeasurementsControl = createClearControl();
-    //olMap.addControl(new ClearMeasurementsControl());            
-    //let FullScreen = new ol.control.FullScreen();
-    //olMap.addControl(FullScreen);
     let Scale = new ol.control.ScaleLine({
         units: 'metric',
         minWidth: 100
     });   
     olMap.addControl(Scale);
-    var selectbase = document.createElement('select');
-    const op1 = document.createElement('option');
-    op1.textContent = 'Satélite (ArcGIS/ESRI';
-    const op2 = document.createElement('option');
-    op2.textContent = 'OpenStreetMap';
-    const op3 = document.createElement('option');
-    op3.textContent = 'Stamen Watercolor';
-    selectbase.appendChild(op1);
-    selectbase.appendChild(op2);
-    selectbase.appendChild(op3);  
-    var handleSelectBase = function(e) {
-        const idx = selectbase.selectedIndex;
-        switch(idx) {
-            case 0:
-                satelitelayer.setVisible(true);
-                omslayer.setVisible(false);
-                stamenlayer.setVisible(false);
-              break;
-            case 1:
-                satelitelayer.setVisible(false);
-                omslayer.setVisible(true);
-                stamenlayer.setVisible(false);
-              break;
-            case 2:
-                satelitelayer.setVisible(false);
-                omslayer.setVisible(false);
-                stamenlayer.setVisible(true);
-                break;                    
-          }
-    };
-    selectbase.addEventListener('change', handleSelectBase, false);
-    var element = document.createElement('div');
-    element.className = 'btn btn-small';
-    element.appendChild(selectbase);
-    
-    
     var btzoomin = document.createElement('button');
     btzoomin.className = "btn btn-small icon-zoom-in";
     btzoomin.setAttribute("title","Acercar");
@@ -185,8 +174,6 @@ function addControlsMap(){
         
     };
     btswiper.addEventListener("click", handleSwiper);   
-
-
     
     var btsave = document.createElement("button");
     btsave.className = "btn btn-small icon-cloud-upload";
@@ -197,24 +184,16 @@ function addControlsMap(){
     elementGroup.className = 'btcontrols btn-group';         
     elementGroup.setAttribute("id","idcontrols");
     
-    
     elementGroup.appendChild(btzoomin);
     elementGroup.appendChild(btzoomout);            
     elementGroup.appendChild(btnorth);
-    
-    
-    //elementGroup.appendChild(searchBarlocal);
     elementGroup.appendChild(btpoligono);
     elementGroup.appendChild(btdistance);
     elementGroup.appendChild(btpoint);
     elementGroup.appendChild(bterase);
     elementGroup.appendChild(btsave);
-    
-    //elementGroup.appendChild(btprint);
-    
     elementGroup.appendChild(btswiper);
     elementGroup.appendChild(btcolorleg);
-    //elementGroup.appendChild(element);
     var groupControl = new ol.control.Control({
         element: elementGroup
     });
@@ -234,4 +213,282 @@ function addControlsMap(){
         layerGroup: basemapsGroup
     });
     olMap.addControl(switcher);
+
+    popup = Ext.create('GeoExt.component.Popup', {
+        map: olMap,
+        width: 140
+    });
+    selectClick = new ol.interaction.Select({                
+        condition: ol.events.condition.click,                
+        layers: (layer) => layer instanceof ol.layer.Vector,
+        hitTolerance: 10,
+    });
+    selectClick.on('select', function (e) {
+        console.log("Select layer");
+        if (delete_handler)
+            document.removeEventListener("keydown", delete_handler);
+
+        if (e.selected.length === 0) {
+            popup.hide();
+            delete_handler = null;
+            return;
+        }
+        delete_handler = function (evt) {
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if (charCode === 46) { // Del key
+                let feature = e.selected[0];
+                if (interactionSource.getFeatures().includes(feature)) {
+                    interactionSource.removeFeature(feature);
+                    let id = feature.getId();
+                    olMap.removeOverlay(measureTooltips[id]);
+                    popup.hide();
+                    delete measureTooltips[id];
+                    selectClick.getFeatures().remove(feature);
+                }
+            }
+        };
+        document.addEventListener('keydown', delete_handler, false);
+        let coordinate = e.mapBrowserEvent.coordinate;
+
+        let message = "<p>";
+        for (const [key, value] of Object.entries(e.selected[0].getProperties())) {
+            if (["bbox", "geometry"].includes(key)) continue;
+            message += key + " ⟶ " + value + "<br>";
+        }
+        message += "</p>";
+        popup.setHtml(message);
+        popup.position(coordinate);
+        if (message !== "<p></p>") popup.show();
+        else popup.hide();
+    });
+    olMap.addInteraction(selectClick);
 }
+
+
+function removeInteraction() {
+    isDrawing = false;
+    olMap.removeInteraction(draw);
+    olMap.un('pointermove', pointerMoveHandler);
+    deleteHelpTooltip();
+}
+
+function measurePointListener() {
+    featureType = 'Point';
+    if (isDrawing) removeInteraction();
+    else addInteraction();
+}
+
+function measureLengthListener() {
+    featureType = 'LineString';
+    if (isDrawing) removeInteraction();
+    else addInteraction();
+}
+
+function measureAreaListener() {
+    featureType = 'Polygon';
+    if (isDrawing) removeInteraction();
+    else addInteraction();
+}
+
+function saveMeasurementsListener() {
+    let serializer = new ol.format.GeoJSON();
+    let features = interactionLayer.getSource().getFeatures();
+
+    if (features.length === 0) {
+        alert("Realice alguna medición antes de guardar");
+        return;
+    }
+
+    let geojson = serializer.writeFeatures(features, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: olMap.getView().getProjection()
+    });
+    console.log(geojson);
+    let geojsonFormat = new ol.format.GeoJSON();
+    let featArray2 = geojsonFormat.readFeatures(geojson, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: 'EPSG:3857'
+    });
+    let redStyle = new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: 6,
+            fill: new ol.style.Fill({
+                color: '#8B0000'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#fff',
+                width: 2
+            })
+        })
+    });
+    let featColl2 = new ol.Collection(featArray2);
+    let vectorLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: featColl2
+        }),
+        style: redStyle
+    });
+    layers.push(vectorLayer);
+    olMap.addLayer(vectorLayer);
+    treePanel.layer.add(vectorLayer);
+
+
+
+    let link = document.createElement('a');
+    let filename = prompt("Escriba un nombre para el archivo (opcional)", "Mediciones " + project_name);
+    if (filename !== null && filename.trim() !== "") link.download = filename + ".geojson";
+    else link.download = "measurements.geojson";
+    let blob = new Blob([geojson], {type: "application/geo+json;charset=utf-8"});
+    console.log(blob); 
+
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+
+function clearMeasurementsListener() {
+    interactionSource.clear();
+    Object.entries(measureTooltips).forEach((tt) => olMap.removeOverlay(tt[1]));
+    selectClick.getFeatures().clear();
+}
+
+var pointerMoveHandler = (evt) => {
+    if (evt.dragging) return;
+    if (drawingFeature) helpMsg = "Doble clic para terminar de dibujar";
+
+    helpTooltipElement.innerHTML = helpMsg
+    helpTooltip.setPosition(evt.coordinate)
+    helpTooltipElement.classList.remove('hidden');
+}
+
+function formatLength(line) {
+    let length = ol.Sphere.getLength(line);
+    if (length > 100) return (length / 1000).toFixed(2) + ' km';
+    else return length.toFixed(2) + ' m';
+}
+
+function formatArea(polygon) {
+    let area = ol.Sphere.getArea(polygon);
+    let hectares = area / 10000;
+    if (area > 10000) return (area / 1000000).toFixed(2) + ' km<sup>2</sup> (' + hectares.toFixed(2) + ' ha)';
+    else return area.toFixed(2) + ' m<sup>2</sup>'
+}
+
+function addInteraction() {
+    helpMsg = 'Clic para comenzar a dibujar';
+    isDrawing = true;
+
+    olMap.getViewport().addEventListener('mouseout', () => {
+        if (helpTooltipElement) helpTooltipElement.classList.add('hidden');
+    });
+    olMap.on('pointermove', pointerMoveHandler);
+
+    draw = new ol.interaction.Draw({
+        source: interactionSource,
+        type: featureType,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(125, 125, 125, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'rgba(0, 0, 0, 0.5)',
+                lineDash: [10, 10],
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 5,
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.7)'
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 255, 255, 0.2)'
+                })
+            })
+        })
+    })
+    olMap.addInteraction(draw)
+
+    try {
+        createMeasureTooltip()
+        createHelpTooltip()
+    } catch (error) {
+        console.log('error', error)
+    }
+    let Medition='1';
+    draw.on('drawstart', (evt) => {
+        numMeasurement++;
+        drawingFeature = evt.feature
+        let tooltipCoord = evt.coordinate
+        listener = drawingFeature.getGeometry().on('change', (evt) => {
+            let geom = evt.target;
+            let output;
+            if (geom instanceof ol.geom.Polygon) {
+                output = formatArea(geom);
+                tooltipCoord = geom.getInteriorPoint().getCoordinates();
+            } else if (geom instanceof ol.geom.LineString) {
+                output = formatLength(geom);
+                tooltipCoord = geom.getLastCoordinate();
+            }
+            measureTooltipElement.innerHTML = output;
+            measureTooltip.setPosition(tooltipCoord);
+            selectClick.getFeatures().clear();
+            Medition= output;
+        })
+    }, this)
+    draw.on('drawend', (evt) => {
+        ans = prompt("Escriba el nombre de la medición (opcional)");
+        if (ans !== null && ans.trim() !== ""){
+            drawingFeature.set("Nombre", ans);
+            drawingFeature.set("Total", Medition);
+            drawingFeature.set("Fecha", new Date().toLocaleDateString());
+        }
+        else{
+            drawingFeature.set("Nombre", featureType);
+            drawingFeature.set("Total", Medition);
+            drawingFeature.set("Fecha", new Date().toLocaleDateString());
+        }
+        measureTooltipElement.className = 'tooltip tooltip-static';
+        measureTooltip.setOffset([0, -7]);
+        drawingFeature.setId(numMeasurement);
+        measureTooltips[numMeasurement] = measureTooltip;
+        drawingFeature = null
+        measureTooltipElement = null
+        ol.Observable.unByKey(listener);
+        removeInteraction();
+        selectClick.getFeatures().clear();
+    }, this)
+}
+
+function deleteHelpTooltip() {
+    if (helpTooltipElement) {
+        helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+        helpTooltipElement = null;
+    }
+}
+
+function createHelpTooltip() {
+    deleteHelpTooltip();
+    helpTooltipElement = document.createElement('div')
+    helpTooltipElement.className = 'tooltip hidden'
+    helpTooltip = new ol.Overlay({
+        element: helpTooltipElement,
+        offset: [15, 0],
+        positioning: 'center-left'
+    })
+    olMap.addOverlay(helpTooltip)
+}
+
+function createMeasureTooltip() {
+    if (measureTooltipElement) {
+        measureTooltipElement.parentNode.removeChild(measureTooltipElement)
+    }
+    measureTooltipElement = document.createElement('div')
+    measureTooltipElement.className = 'tooltip tooltip-measure'
+    measureTooltip = new ol.Overlay({
+        element: measureTooltipElement,
+        offset: [0, -15],
+        positioning: 'bottom-center'
+    })
+    olMap.addOverlay(measureTooltip)
+}
+
